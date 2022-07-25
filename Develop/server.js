36ac88ca9d0b9d32e5da0c374bcc5552ead473b3
fuaus <kotingfunk@gmail.com>
2022-07-25 18:50:51 -0400

    create htmlRoutes.js: read function for html routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,20 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes")
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+// complete detail parsing, then convert to JSON
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.use(htmlRoutes)
+
+app.use(apiRoutes)
+
+app.listen(PORT,()=>{
+    console.log("App is listening on Port http://localhost:"+ PORT)
+});
