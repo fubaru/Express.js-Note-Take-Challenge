444fbe9f7ec9a7a37ce3978001ab34e8cfea20f1
Fuba <kotingfunk@gmail.com>
2022-07-27 10:58:12 -0400

    add comments

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,18 @@
+// import dependencies
 const router = require("express").Router()
 const fs = require("fs")
+// make it cleaner, code refractor
 const db=require("../db/db.json")
 
+// route for reading db.json file, then return saved notes as json
 router.get("/api/notes", (req,res)=>{
     res.json("db")
 })
 
+// new note received and save to the req.body, add it to db.json file, then return new notes to client
 router.post("/api/notes", (req,res)=>{
     console.log(req.body)
+    
     let newNote = req.body;
     let noteList = JSON.parse(fs.readFileSync(db, "utf8"));
     let notelength = (noteList.length).toString();
@@ -18,5 +23,5 @@ router.post("/api/notes", (req,res)=>{
     fs.writeFileSync(db, JSON.stringify(noteList));
     res.json(noteList);
 });
-
+// export htmlRoutes file
 module.exports=router
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,12 +1,13 @@
+// import dependencies
 const router = require("express").Router()
 const path=require("path")
-
+// route to notes page
 router.get("/notes",(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/notes.html'))
 })
-
+// wild card, route to homepage index.html
 router.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
-
+// export htmlRoutes file
 module.exports=router;
