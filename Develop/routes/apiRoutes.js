77c6a5cacf6c26aff9c375baa8b03f5d1367422a
Fuba <kotingfunk@gmail.com>
2022-07-27 10:52:56 -0400

    apiRoutes.js: add logic for router.post and push any new notes entry into req.body and then write the data to db.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,12 +2,21 @@ const router = require("express").Router()
 const fs = require("fs")
 const db=require("../db/db.json")
 
-router.get("/api/notes",(req,res)=>{
+router.get("/api/notes", (req,res)=>{
     res.json("db")
 })
 
 router.post("/api/notes", (req,res)=>{
-    
-})
+    console.log(req.body)
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync(db, "utf8"));
+    let notelength = (noteList.length).toString();
+
+    newNote.id = notelength;
+    noteList.push(newNote);
+
+    fs.writeFileSync(db, JSON.stringify(noteList));
+    res.json(noteList);
+});
 
 module.exports=router
