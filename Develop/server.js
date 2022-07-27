0bd345271f83eef3406743b077c7b45b7eef25a5
Fuba <kotingfunk@gmail.com>
2022-07-27 09:16:48 -0400

    add gitignore files for heroku deployment purposes and add comments in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,20 @@
+// import express for routes
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes")
+// process.env needed for deployment to Heroku
 const PORT = process.env.PORT || 3001;
-
+// store express in a const var for cleaner code
 const app = express();
-
+// needed to make it available for web
 app.use(express.static("public"));
-// complete detail parsing, then convert to JSON
+// complete detail parsing, then convert to JSON; Middleware Parsing
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-
+// Module route for HTML
 app.use(htmlRoutes)
-
+// Module route for api
 app.use(apiRoutes)
 
 app.listen(PORT,()=>{
