067431e3ca06e835afb6fc5755edf1e752a9a745
Fuba <kotingfunk@gmail.com>
2022-07-27 14:34:15 -0400

    adjust router.post logic in apiroutes.js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,27 +1,45 @@
 // import dependencies
 const router = require("express").Router()
 const fs = require("fs")
-// make it cleaner, code refractor
+
 const db=require("../db/db.json")
+const uniqid = require("uniqid")
 
 // route for reading db.json file, then return saved notes as json
 router.get("/api/notes", (req,res)=>{
-    res.json("db")
+    res.json(db)
 })
 
 // new note received and save to the req.body, add it to db.json file, then return new notes to client
 router.post("/api/notes", (req,res)=>{
     console.log(req.body)
+
+    // let newNote = {
+    //     id: uniqid(),
+    //     title: req.body.title,
+    //     text: req.body.text
+    // }
+    let newNote = req.body
+    let data = fs.readFileSync("./db/db.json", "utf8")
+    let noteList = JSON.parse(data);
     
-    let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync(db, "utf8"));
-    let notelength = (noteList.length).toString();
+    noteList.push(newNote)
 
-    newNote.id = notelength;
-    noteList.push(newNote);
-
-    fs.writeFileSync(db, JSON.stringify(noteList));
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
+
+    // fs.readFile("./db/db.json", (err,data) => {
+    //     if (err) throw err
+    //     let newData = JSON.parse(data)
+
+    //     newData.push(newNote);
+
+    //     fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+    //         if (err) throw err;
+    //         res.send('Success')
+    //     })
+    // });
+
 });
 // export htmlRoutes file
 module.exports=router
